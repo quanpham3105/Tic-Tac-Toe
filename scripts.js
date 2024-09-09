e8bf99dae2d5b8ac81141fe9c57065c531f4761a
quanpham3105 <quanpham3105@gmail.com>
2024-09-09 08:27:11 -0700

    Made tic tac toe outline

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const b1 = document.getElementById("b1");
+const b2 = document.getElementById("b2");
+const b3 = document.getElementById("b3");
+const b4 = document.getElementById("b4");
+const b5 = document.getElementById("b5");
+const b6 = document.getElementById("b6");
+const b7 = document.getElementById("b7");
+const b8 = document.getElementById("b8");
+const b9 = document.getElementById("b9");
+
+[b1, b2, b3, b4, b5, b6, b7, b8, b9].forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.value == "") {
+            button.value = "X";
+        }
+        if (button.value)
+    });
+});
